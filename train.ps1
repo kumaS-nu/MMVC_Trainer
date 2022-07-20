129339dc582192f855fa1d7cd5f5caeb62b70538
kumaS-nu <kumachan0220ios@gmail.com>
2022-07-20 16:13:26 +0900

    fix bug

--- train.ps1
+++ train.ps1
@@ -1,11 +1,18 @@
-[System.IO.FileInfo[]] $files = dir "configs" -File
-mkdir "models"
 ./env/Scripts/Activate.ps1
+cd monotonic_align
+python setup.py build_ext --inplace
+cd ..
+
+[System.IO.FileInfo[]] $files = dir "configs" -File
+
 foreach ($f in $files) {
     if ($f.Name.Contains("baseconfig")) {
         continue
     }
-    $model_dir = "models" + $f.Name
+    if ($f.Name.Contains("gitignore")){
+        continue
+    }
+    $model_dir = "models/" + [System.IO.Path]::GetFileNameWithoutExtension($f.Name)
     mkdir $model_dir
     $c = "configs/" + $f.Name
     python train_ms.py -c $c -m $model_dir -fg fine_model/G_180000.pth -fd fine_model/D_180000.pth
